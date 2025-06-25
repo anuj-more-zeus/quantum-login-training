92c7fd37564f0fe5be6bf82ffbde583f4926410d
Anuj More <moreofme7777@gmail.com>
2025-06-25 13:46:24 +0530

    Convert 'state' select label to get state data dynamically.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from "./navbar/navbar";
 import { Footer } from "./footer/footer";
+import { LoginCard } from "./login-card/login-card";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, LoginCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/login-card/login-card.ts
+++ src/app/login-card/login-card.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { StateDataService } from '../services/StateDataService';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-card',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-card.html',
   styleUrl: './login-card.css'
 })
 export class LoginCard {
+  private stateDataService = inject(StateDataService);
 
+  stateDistrictData: { [key: string]: string[] } = {};
+  states: string[] = [];
+  districts: string[] = [];
+
+  selectedState: string = '';
+  selectedDistrict: string = '';
+  
+  ngOnInit(): void {
+    this.stateDataService.getStatesAndDistrict().subscribe(data => {
+      this.stateDistrictData = data;
+      this.states = Object.keys(data);
+    })
+  }
+
+  onStateChange(): void {
+    this.districts = this.stateDistrictData[this.selectedState] || [];
+    this.selectedDistrict = '';
+  }
 }
