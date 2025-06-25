54b478a899ab8e0466f1254e5f0b85636d38f7d3
Anuj More <moreofme7777@gmail.com>
2025-06-25 11:52:41 +0530

    Create 'footer' markup and css.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from "./navbar/navbar";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
